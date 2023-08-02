a8aed1b581ba7bb998071680541112ee736f0e67
Influenzo <52459025+vasylcode@users.noreply.github.com>
2023-08-01 18:15:13 -0600

    proposals added

--- index.sh
+++ index.sh
@@ -74,5 +74,39 @@ function show_validators_list() {
 }
 
 function show_proposals_list() {
+	proposals_response=$(curl -s "$API_URL"/gov/v1beta1/proposals | tr -d '\n')
 
+	if [ -z "$proposals_response" ]; then
+		echo "Failed to fetch data from the API. Please check your connection or the API endpoint."
+		exit 1
+	fi
+
+	proposals_info=$(echo "$proposals_response" | jq --raw-output '.proposals | map({id: .proposal_id, title: .content.title, description: .content.description, status: .status, submit_time: .submit_time, deposit: .total_deposit.amount, denom: .total_deposit.denom})')
+
+	if [ -z "$proposals_info" ]; then
+		echo "No validators found in the response."
+		exit 1
+	fi
+
+	echo "Zetachain Proposals Information:"
+	echo "================================"
+	echo
+	echo "$proposals_info" | jq -c '.[]' | while read -r object; do
+		id=$(echo "$object" | jq -r '.id')
+		title=$(echo "$object" | jq -r '.title')
+		description=$(echo "$object" | jq -r '.description')
+		status=$(echo "$object" | jq -r '.status')
+		submit_time=$(echo "$object" | jq -r '.submit_time')
+		deposit=$(echo "$object" | jq -r '.deposit')
+		denom=$(echo "$object" | jq -r '.denom')
+		echo "ID: $id"
+		echo "Title: $title"
+		echo "Description: $description"
+		echo "Status: $status"
+		echo "Submit Time: $submit_time"
+		echo "Deposit: $deposit $denom"
+		echo "----------------------"
+	done
+	echo
+	echo "================================"
 }
